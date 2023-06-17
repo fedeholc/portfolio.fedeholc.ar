b965376796aef8035e8c263969af6f7933d34b60
fedeholc <federicoholc@gmail.com>
2023-06-17 10:38:21 -0300

    new layout

---
+++ app/page copy.js
@@ -0,0 +1,82 @@
+import { archivoBlack } from "./layout";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex flex-col  justify-around  ">
+      <div className="h-full flex flex-col  ">
+        <header className="flex items-center flex-col text-center p-4 gap-5">
+          <div className="flex flex-row items-center justify-center gap-4">
+            <Image
+              className="fotosperfil rounded-full border-2 border-black mt-4 mb-1"
+              src="/fp.jpg"
+              width="150"
+              height="150"
+            />
+            <h1>hola =)</h1>
+          </div>
+          <div>
+            <h2 className="borde_amarillo">Sitio en construcción</h2>
+          </div>
+        </header>
+        <section className="flex flex-col items-center mt-4 text-center">
+          <h3>Mientras tanto podés encontrame en:</h3>
+          <div className="flex flex-col items-start">
+            <p className="mt-4 mb-1">
+              ↳ Github:
+              <a
+                target="_blank"
+                className="linktext"
+                href="https://github.com/fedeholc/"
+              >
+                <span> github.com/fedeholc</span>
+              </a>
+            </p>
+            <p className="mt-2 mb-1">
+              ↳ LinkedIn:
+              <a
+                target="_blank"
+                className="linktext"
+                href="https://www.linkedin.com/in/federicoholc/"
+              >
+                <span> linkedin.com/in/federicoholc</span>
+              </a>
+            </p>
+            <p className="mt-2 mb-1">
+              ↳ Mi blog de aprendizaje:
+              <a
+                target="_blank"
+                className="linktext"
+                href="https://nsp.fedeholc.ar/"
+              >
+                <span> nsp.fedeholc.ar</span>
+              </a>
+            </p>
+            <p className="mt-2 mb-1">
+              ↳ O mandarme un mail a:
+              <a
+                target="_blank"
+                className="linktext"
+                href="mailto:dev@fedeholc.ar"
+              >
+                <span> dev@fedeholc.ar</span>
+              </a>
+            </p>
+            <p className="mt-2 mb-1">
+              ↳ También hago fotos:
+              <a
+                target="_blank"
+                className="linktext"
+                href="https://www.instagram.com/fedeholc/"
+              >
+                <span> instagram.com/fedeholc</span>
+              </a>
+            </p>
+          </div>
+        </section>
+      </div>
+      
+      <footer>Federico Holc ⚡️ 2023</footer>
+    </main>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,81 +1,85 @@
-import { archivoBlack } from "./layout";
+import "./globals.css";
+import { Archivo_Black, Roboto_Mono } from "next/font/google";
+import { Recursive, Montserrat } from "next/font/google";
 import Image from "next/image";
+import Fotos from "./fotos";
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col  justify-around  ">
-      <div className="h-full flex flex-col  ">
-        <header className="flex items-center flex-col text-center p-4 gap-5">
-          <div className="flex flex-row items-center justify-center gap-4">
-            <Image
-              className="fotosperfil rounded-full border-2 border-black mt-4 mb-1"
-              src="/fp.jpg"
-              width="150"
-              height="150"
-            />
-            <h1>hola =)</h1>
+    <div className="main__grid">
+      <div className="navbar">Fede Github | Proyectos | Blog</div>
+
+      <main>
+        <div className="main-area">
+          <h2>¡Hola! =)</h2>
+          <div className="main-container">
+            <div>
+              <p>
+                Mi nombre es Federico Holc y soy un desarrollador web front-end,
+                con base en Buenos Aires, Argentina.
+              </p>
+              <p>
+                Trabajo principalmente con HTML, CSS, JavaScript y React, así
+                como también con NextJS, Node, y bases de datos SQL o MongoDB
+                cuando los proyectos lo requieren.
+              </p>
+              <p>
+                Cuando no estoy programando o aprendiendo cosas nuevas, me
+                dedico a la fotografía.
+              </p>
+            </div>
+            <div className="foto-container">
+              <Fotos></Fotos>
+              {/*  <Image
+                alt="Foto de la cara de federico holc"
+                className="fotosperfil rounded-full border-2 border-black"
+                src="/ff2.jpg"
+                width="200"
+                height="200"
+                id="foto1"
+              />
+              <Image
+                alt="Foto de la cara de federico holc"
+                className="fotosperfil rounded-full border-2 border-black"
+                src="/fp.jpg"
+                width="200"
+                height="200"
+                id="foto2"
+              /> */}
+            </div>
           </div>
-          <div>
-            <h2 className="borde_amarillo">Sitio en construcción</h2>
+          <div className="  flex flex-wrap flex-row place-items-baseline">
+            <span>Podés ver mis&nbsp;</span>
+            <span>
+              <button className="boton-encontrar">Proyectos</button>,&nbsp;
+            </span>
+            <span>descargar mi&nbsp;</span>
+            <span>
+              <button className="boton-encontrar">CV en PDF</button>,&nbsp;
+            </span>
+            <span>
+              <span>leer&nbsp;</span>
+              <button className="boton-encontrar">NSP Blog</button>,&nbsp;
+            </span>
+            <span>
+              <span>encontrarme en:&nbsp;</span>
+              <button className="boton-encontrar">GitHub</button>&nbsp;
+            </span>
+            <span>
+              <button className="boton-encontrar">LinkedIn</button>&nbsp;
+            </span>
+            <span>
+              <button className="boton-encontrar">Instagram</button>,&nbsp;
+            </span>
+            <span>
+              <span>o escribirme a:&nbsp;</span>
+              <button className="boton-encontrar">dev@fedeholc.ar</button>.
+            </span>
           </div>
-        </header>
-        <section className="flex flex-col items-center mt-4 text-center">
-          <h3>Mientras tanto podés encontrame en:</h3>
-          <div className="flex flex-col items-start">
-            <p className="mt-4 mb-1">
-              ↳ Github:
-              <a
-                target="_blank"
-                className="linktext"
-                href="https://github.com/fedeholc/"
-              >
-                <span> github.com/fedeholc</span>
-              </a>
-            </p>
-            <p className="mt-2 mb-1">
-              ↳ LinkedIn:
-              <a
-                target="_blank"
-                className="linktext"
-                href="https://www.linkedin.com/in/federicoholc/"
-              >
-                <span> linkedin.com/in/federicoholc</span>
-              </a>
-            </p>
-            <p className="mt-2 mb-1">
-              ↳ Mi blog de aprendizaje:
-              <a
-                target="_blank"
-                className="linktext"
-                href="https://nsp.fedeholc.ar/"
-              >
-                <span> nsp.fedeholc.ar</span>
-              </a>
-            </p>
-            <p className="mt-2 mb-1">
-              ↳ O mandarme un mail a:
-              <a
-                target="_blank"
-                className="linktext"
-                href="mailto:dev@fedeholc.ar"
-              >
-                <span> dev@fedeholc.ar</span>
-              </a>
-            </p>
-            <p className="mt-2 mb-1">
-              ↳ También hago fotos:
-              <a
-                target="_blank"
-                className="linktext"
-                href="https://www.instagram.com/fedeholc/"
-              >
-                <span> instagram.com/fedeholc</span>
-              </a>
-            </p>
-          </div>
-        </section>
-      </div>
+        </div>
+      </main>
+
       <footer>Federico Holc ⚡️ 2023</footer>
-    </main>
+    </div>
   );
 }
