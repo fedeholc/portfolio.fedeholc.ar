04b02421548ad2df25daf30e6faff2dbf6415bc3
fedeholc <federicoholc@gmail.com>
2023-06-20 16:05:39 -0300

    update css

--- app/page.js
+++ app/page.js
@@ -76,42 +76,48 @@ export default function Home() {
                 Argentina.
               </p>
               <p>
-                De muy chico comencé a jugar <span className="emoji">👾</span>{" "}
-                space invaders <span className="emoji">👾</span> en una XT286
-                que había en casa, y di mis primeros pasos en la programación
-                usado Logo <span className="emoji">🐢</span>. Para el último año
-                de la escuela secundaria (técnica en computación) ya estaba
-                trabajando como desarrollador en Visual Basic. Luego comencé a
-                estudiar Ciencias de la Computación en la UBA. Pero era el año
-                2001 y en la Argentina y en mi vida hubo un "plot twist"{" "}
-                <span className="emoji">🙈</span>, o por qué no, un ¡epic fail!
-                <span className="emoji">💩</span> Dejé la programación.
-                <span className="emoji">🤦🏻😭☠️</span>
-              </p>
-              <p>
-                En los años que siguieron fuí sociólogo, y profesor en escuelas
-                secundarias y en la UBA. Allí aprendí y enseñé todo lo que pude.
-                <span className="emoji">👨‍🏫</span>
-              </p>
-              <p>
-                Hace un tiempo, a partir de utilizar software fotográfico de
-                código abierto (¡también estudié fotografía!
-                <span className="emoji">📸</span>), me volvió a interesar
-                profundamente la programación. Me encontré con que el desarrollo
-                web era algo muy distinto al de hace 20 años{" "}
-                <span className="emoji">🤯</span>, ¡me atrapó!{" "}
-                <span className="emoji">🕸️</span>, y desde entonces no paré de
-                aprender y programar (ahora también con una mirada puesta en las
-                necesidades de usuarios y usuarias, que antes no tenía). ¿Qué
-                mejor que hacerlo a tiempo completo ayudando a una organización
-                a alcanzar sus objetivos?<span className="emoji"> 🤝</span>
+                De chico amaba jugar <span className="emoji">👾</span>{" "}
+                space invaders <span className="emoji">👾</span> en la XT286 de
+                mi papá, y di mis primeros pasos en la programación usado Logo{" "}
+                <span className="emoji">🐢</span>. Para el último año de la
+                escuela secundaria (técnica en computación) ya estaba trabajando
+                como desarrollador en Visual Basic. Luego comencé a estudiar
+                Ciencias de la Computación en la UBA.
               </p>
             </div>
             <div className="foto-container">
               <Fotos></Fotos>
             </div>
           </div>
-          <div className="  flex flex-wrap flex-row place-items-baseline">
+          <div>
+            {" "}
+            <p>
+              Pero era el año 2001 y en la Argentina y en mi vida hubo un "plot
+              twist"
+              <span className="emoji">🙈</span>, o por qué no, un ¡epic fail!
+              <span className="emoji">💩</span> Dejé la programación.
+              <span className="emoji">🤦🏻😭☠️</span>
+            </p>
+            <p>
+              En los años que siguieron fuí sociólogo, y profesor en escuelas
+              secundarias y en la UBA. Allí aprendí y enseñé todo lo que pude.
+              <span className="emoji">👨‍🏫</span>
+            </p>
+            <p>
+              Hace un tiempo, a partir de utilizar software fotográfico de
+              código abierto (¡también estudié fotografía!
+              <span className="emoji">📸</span>), me volvió a interesar
+              profundamente la programación. Me encontré con que el desarrollo
+              web era algo muy distinto al de hace 20 años{" "}
+              <span className="emoji">🤯</span>, ¡me atrapó!{" "}
+              <span className="emoji">🕸️</span>, y desde entonces no paré de
+              aprender y programar (ahora también con una mirada puesta en las
+              necesidades de usuarios y usuarias, que antes no tenía). ¿Qué
+              mejor que hacerlo a tiempo completo ayudando a una organización a
+              alcanzar sus objetivos?<span className="emoji"> 🤝</span>
+            </p>
+          </div>
+          <div className="  flex flex-wrap flex-row place-items-baseline mt-8">
             <span>Podés ver mis&nbsp;</span>
             <span>
               <button className="boton-encontrar">
