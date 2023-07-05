960b87524af5517b232844cf1e7f69bd750fbf00
Federico Holc <134601367+holcf@users.noreply.github.com>
2023-07-05 14:08:25 +0000

    update proyectos

--- app/proyectos/insta/page.js
+++ app/proyectos/insta/page.js
@@ -7,11 +7,12 @@ import screenshot1 from "@public/proyecto-nsp/Screenshot from 2023-06-26 20-42-0
 
 export default function ProyectoInsta() {
   const TEXTO = {
-    titulo: `x`,
-    bajada: `x`,
+    titulo: `insta`,
+    bajada: `Es una extensión muy sencilla para el navegador Chrome que permite ver las fotos de instagram a máxima resolución.`,
     stack: (
       <ul>
-        <li></li>
+        <li>
+- HTML, CSS, JavaScript, Chrome API</li>
         <li></li>
         <li></li>
         <li></li>
@@ -28,11 +29,18 @@ export default function ProyectoInsta() {
     linkWebText: `xxxx`,
     linkWebHref: `xxxxxxx`,
     subtitulo1: `Objetivo`,
-    texto1: `xxxxx`,
+    texto1: `Quienes amamos ver y hacer fotos, disfrutamos de poder apreciarlas a máxima resolución y en pantalla completa. La extensión busca resolver el problema ese sin tener que recurrir a otra plataforma (¡tal vez nunca surja un nuevo flickr! ) y de una manera sencilla.
+
+    Al tener la extensión activada, en el angulo superior derecho de cada foto aparece de manera sutil un icono de una lupa. Al hacer click sobre el icono se abre la imagen a tamaño completo y sin otras distracciones. Luego clickeando sobre la misma se regresa a la página y posición original.
+    `,
     subtitulo2: `Características y funcionalidades`,
     texto2: (
       <ul>
-        <li>
+        <li>Ideas para implementar a futuro:
+
+- Que se pueda elegir si se abre la imagen en una nueva pestaña o en la misma, y si se utiliza o no el modo de pantalla completa del navegador.
+- Que se pueda generar una galería de imágenes para ir viendo una trás otra a pantalla completa evitando tener que clickear una por una de las que queremos ver.
+
         </li>
         <li>
         </li>
@@ -47,8 +55,15 @@ export default function ProyectoInsta() {
     texto3: (
       <ul>
         <li>
+- La principal dificultad técnica con la que me encontré es que al tratarse Instagram de una web dinámica, a medida que se van cargando nuevos elementos en el feed, es necesario ir agregando los listeners e iconos de lupas. Pero no encontré un evento que se dispare cuando aparece nuevo contenido. La solución que implementé es hacerlo con el evento mousemove, lo cuál es efectivo pero poco eficiente ya que implica leer toda la estructura de la web muchas más veces de las necesarias. Tengo anotado coomo tarea pendiente encontrar una solución más eficiente. (Si usted que está que está leyendo esto tiene la respuesta ¡por favor escribame!)
+- Otra cuestión que aún debo investigar es como portar la extensión a Firefox, Safari y Edge, de modo tal de no excluir a quiénes utilizan otros de los principales navegadores.
+- Finalmente mencionar otra dificultad no técnica que ingenuamente no había tenido en cuenta: luego de varias semanas de publicada la extensión me llegó un aviso de que la eliminaban de la chrome store por utilizar la palabra "Instagram" en el nombre y porque el logo incluía los colores de la marca.
+
         </li>
-        <li>
+        <li>- Hacer extensiones del navegador es un buen ejercicio para practicar HTML, CSS y JavaScript en estado puro, y en este caso también para aprender sobre la manipulación del DOM y ver cómo es su estructura en este tipo de webs. De hecho hay que estar atento a cualquier cambio que pueda hacer que deje de funcionar la extensión (nota mental: podría programar una alerta que me avise cuando ese ocurre).
+- No hay tantos contenidos para aprender a desarrollar extensiones por lo que valoro mucho la dedicación a elaborar una buena documentación como la que tiene Chrome, con explicaciones y ejemplos.
+- Es muy motivador poder hacer con pocas líneas de código algo que funciona, que es útil y que se puede compartir con otras personas. Me ha dado ganas e ideas para hacer montones de extensiones (¿alguién me quiere donar tiempo?).
+
         </li>
         <li>
         </li>
--- app/proyectos/tv/page.js
+++ app/proyectos/tv/page.js
@@ -7,20 +7,24 @@ import screenshot1 from "@public/proyecto-nsp/Screenshot from 2023-06-26 20-42-0
 
 export default function ProyectoTV() {
   const TEXTO = {
-    titulo: `x`,
-    bajada: `x`,
+    titulo: `MyTvShows`,
+    bajada: `MyTvShows es una aplicación que permite llevar registro de tus series favoritas y ver facilmente cuáles tienen nuevos episodios.`,
     stack: (
       <ul>
+        <li>React / JavaScript</li>
+        <li>HTML / Tailwind CSS</li>
+        <li>Firebase</li>
+        <li>Git / Github</li>
         <li></li>
         <li></li>
         <li></li>
         <li></li>
         <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
+        <li>La verdad es que elegí React porque estaba comenzando a aprenderlo y me pareció un buena oportunidad para poner en práctica cuestiones básicas como el uso de componentes,renderizado condicional, manejo de estados y props, etc.
+Debo confesar que no sé la causa particular pero me sentí muy a gusto trabajando con React al punto de decidir que quería profundizar más en su estudio y dejar de lado Angular que es con lo que había trabajado antes.
+También aproveche para probar por primera vez Tailwind como framework de CSS. Me pareció muy interesante utility clases.
+Por último, utilicé una base de datos de Firebase ya que era muy sencilla de implementar y no tenía grandes requerimientos de almacenamiento.
+</li>
       </ul>
     ),
     linkCodeText: `github/fedeholc  xxx`,
@@ -28,11 +32,19 @@ export default function ProyectoTV() {
     linkWebText: `xxxx`,
     linkWebHref: `xxxxxxx`,
     subtitulo1: `Objetivo`,
-    texto1: `xxxxx`,
+    texto1: `La aplicación fue pensada para resolver un problema aparentemente sencillo como el de tener un registro de las series que uno ve en un solo lugar y poder ver si hay nuevos episodios sin depender de tener acceso a las distintas plataformas que existen hoy en día.`,
     subtitulo2: `Características y funcionalidades`,
     texto2: (
       <ul>
-        <li>
+        <li>La aplicación permite buscar series en la base de datos de IMDB mediante el uso de una API, y agregarlas a la lista de favoritos.
+Se puede ver un listado de esas series con la información de la cantidad de temporadas y la fecha de emisión del último episodio.
+Es posible ordenar la lista por la fecha de último episodio justamente para ver cuáles son los que han salido más recientemente.
+
+
+> Ideas para implementar a futuro:
+
+- Sin dudas la aplicación es muy básica y podría agregarse gran cantidad de funcionalidades. Pero en particular, ahora que sé como desarrollar extensiones para el navegador, creo que sería muy util hacer una que nos muestre una alerta cuando haya nuevos episodios de nuestras series favoritas.
+- Para eso seria necesario también que la aplicación le permita al usuario indicar cuál es el último capitulo de la serie que ha visto.
         </li>
         <li>
         </li>
@@ -46,7 +58,10 @@ export default function ProyectoTV() {
     subtitulo3: "Problemas y aprendizajes",
     texto3: (
       <ul>
-        <li>
+        <li>El principal desafio fue combinar distintas funciones asincrónicas para que funcionaran en conjunto, ya que primero debía leer los datos de Firebase, luego obtener cierta información de las series desde la API de OpenIMDB, y luego con esa información realizar una nueva consulta a otro endpoint de la misma API para obtener la información de los episodios de cada serie.
+Afortunadamente pude resolverlo correctamente con la utilización de async/await. Aunque me queda pendiente profundizar en como abordar los errores cuando algo falla en una de las funciones asincrónicas.
+También tuve algunos dolores de cabeza con el UseEffect en relación a la carga de los datos, pero la nueva documentación de React es muy clara en cuanto a cómo usarlo y sobre todo cuando no usarlo, así que pude resolverlo.
+
         </li>
         <li>
         </li>
