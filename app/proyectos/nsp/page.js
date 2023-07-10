e90baf70175cec1bdae5c3050b363dce7e5f6632
Federico Holc <134601367+holcf@users.noreply.github.com>
2023-07-10 12:33:48 +0000

    update blog

--- app/proyectos/nsp/page.js
+++ app/proyectos/nsp/page.js
@@ -22,61 +22,43 @@ export default function ProyectoNSP() {
     linkCodeHref: `https://www.github.com/fedeholc/NuncaSupeProgramar`,
     linkWebText: `nsp.fedeholc.ar`,
     linkWebHref: `https://nsp.fedeholc.ar/`,
-    subtitulo1: `Objetivo`,
-    texto1: `Si hay algo que aprendí como docente es que -junto con la práctica-, intentar enseñar o explicar algo (y si es por escrito mejor) es un excelente camino para aprenderlo. También es importante el intercambio, que genera un aprendizaje colectivo. Por ello decidí crear un blog, que es una gran tecnología para la difusión de conocimiento: a diferencia de los contenidos que circulan por plataformas cerradas, sus contenidos puede ser indexados por los buscadores, compartidos en redes sociales, y estar a disposición de quienes lo necesiten.`,
-    subtitulo2: `Características y funcionalidades`,
+    subtitulo1: `¿Para qué?`,
+    texto1: `Algo que aprendí como docente es que junto con la práctica, intentar enseñar o explicar algo (y si es por escrito mejor) es un excelente camino para aprenderlo. También es importante el aprendizaje colectivo que se genera con el intercambio. Por ello un blog, que es una excelente tecnología para la difusión del conocimiento: a diferencia de lo que circula por plataformas cerradas, sus contenidos puede ser indexados por los buscadores, compartidos en redes sociales, y estar a disposición de quienes lo necesiten.`,
+    subtitulo2: `¿Qué tiene de bueno?`,
     texto2: (
       <ul>
         <li>
-          Gracias a NextJS el blog se genera como sitio estático del lado del
-          servidor lo cual hace que sea veloz, y muy fácil de indexar por los
-          buscadores. También es muy sencillo de publicar, ya que al alojar el
-          blog en Vercel, el deploy se realiza automáticamente cada vez que se
-          actualiza el repositorio en Github.
+          Mediante Next.js elo blog se genera como sitio estático del lado del servidor lo cual hace que sea veloz, y fácil de indexar por los buscadores. También es sencillo de publicar, ya que al estar alojado en Vercel, el deploy se realiza automáticamente cada vez que se actualiza el repositorio en Github.
         </li>
         <li>
-          Es muy fácil publicar: se guardar el contenido del post en un archivo
+          Añadir nuevas entrada al blog es simple: se escribe el contenido del post en un archivo
           con formato MarkDown, se lo sube a una carpeta dentro del repositorio
-          de proyecto, y automáticamente se realiza el deploy a Vercel.
+          de proyecto, y automáticamente se realiza su publicación.
         </li>
         <li>
-          Utilizando el formato MarkDown junto con la librería remark.js se le
-          puede dar formato resaltado a los bloques de código que haya en cada
-          publicación.
+          Utilizando el formato MarkDown junto con la librería remark.js se le puede dar formato resaltado a los bloques de código de las publicaciones.
         </li>
         <li>
-          Es responsive y se adapta para que pueda ser leído desde cualquier
-          dispositivo.
+          Es responsive y se adapta para que pueda ser leído desde cualquier dispositivo.
         </li>
         <li>
-          Funcionalidades para implementar a futuro: realización de búsquedas,
-          filtrado por etiquetas, comentarios en las publicaciones, etc.
+          Funcionalidades para implementar a futuro: realización de búsquedas dentro del sitio,
+          filtrado de publicaciones por etiquetas, posibilidad de añadir comentarios, etc.
         </li>
       </ul>
     ),
-    subtitulo3: "Problemas y aprendizajes",
+    subtitulo3: "Problemas en el camino, y aprendizajes",
     texto3: (
       <ul>
         <li>
-          Dado que solo había programando SPAs con renderizado del lado del
-          cliente, me requirió un tiempo comprender las variantes que hay para
-          hacerlo del lado del servidor (o de manera combinada), y cómo trabajar
-          de ese forma con NextJS para integrar frontend y backend en una sola
-          aplicación full stack.
+          Hasta aquí solo había programando SPAs con renderizado del lado del cliente, por lo que me llevo un tiempo comprender las variantes que hay para hacerlo del lado del servidor (o de manera combinada), y cómo trabajar de ese modo con Next, integrando frontend y backend en una sola aplicación full stack.
         </li>
         <li>
-          Justo al poco tiempo de haber publicado el blog, Next lanzó una nueva
-          versión incorporando un nuevo router y utilizando React Server
-          Components, y cometí el error de ir tras la novedad y actualizar a la
-          nueva versión, porque no logré hacer que funcionaran las librerías
-          para el estilizado del MarkDown. Por lo que tuve que volver a la
-          versión anterior, pero con la lección aprendida de que en la mayoría
-          de los casos hay que utilizar tecnologías establecidas y probadas.
+          Al poco tiempo de publicado el blog, Next lanzó una nueva versión incorporando React Server Components, y cometí el error de ir tras la novedad, ya que no logré hacer que funcionaran las librerías
+          para el estilizado del MarkDown en la nueva versión. Tuve que regresar a la  anterior, pero con la lección aprendida sobre la importancia de utilizar tecnologías establecidas y probadas.
         </li>
         <li>
-          Publicar el blog en un dominio propio me llevó también a aprender como
-          registrarlo, cómo manejar los DNS, y las configuraciones necesarias
-          para que pueda ser indexado por los buscadores.
+          Publicar el blog en un dominio propio me llevó también a aprender como registrarlo, cómo manejar los DNS, y las configuraciones necesarias para que pueda ser indexado por los buscadores.
         </li>
       </ul>
     ),
