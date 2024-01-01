41e129999c7662b92592b3532c3fb529556813cd
fede <riosdelimon@gmail.com>
2024-01-01 23:01:10 +0700

    Remove unused imports in goToTop.js

--- app/components/goToTop.js
+++ app/components/goToTop.js
@@ -1,6 +1,6 @@
 import Link from "next/link";
-import goTop from "@public/gotop.png";
-import Image from "next/image";
+/* import goTop from "@public/gotop.png";
+import Image from "next/image"; */
 import goToTop from "./goToTop.module.css";
 
 export default function GoToTop() {
