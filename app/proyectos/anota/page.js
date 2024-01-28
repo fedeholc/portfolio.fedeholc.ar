97d729300bc2da870f5fb6176aa5a235b3732c39
fedeholc <federicoholc@gmail.com>
2024-01-28 17:21:22 -0300

    Update ANOTÁ! (Another Note Taking App) screenshots and descriptions

--- app/proyectos/anota/page.js
+++ app/proyectos/anota/page.js
@@ -3,38 +3,39 @@ import "@app/globals.css";
 import Link from "next/link";
 import Image from "next/image";
 import externalLinkIcon from "@public/external-link.png";
-import screenshot1 from "@public/proyecto-anota/anota-layout.webp";
-import screenshot2 from "@public/proyecto-anota/anota-extension.webp";
-import screenshot3 from "@public/proyecto-anota/anota-notas.webp";
+import screenshot1 from "@public/proyecto-anota/anota_inicio.webp";
+import screenshot2 from "@public/proyecto-anota/anota_highlighter.webp";
+import screenshot3 from "@public/proyecto-anota/anota_etiquetas.webp";
 import herb from "@public/herb.png";
 
 export default function ProyectoANOTA() {
   const TEXTO = {
-    titulo: `ANOTÁ! (Another Note Taking App)`,
-    bajada: `¿Otra aplicación más para tomar notas? ¡Así es, otra más! Creada para aprender a hacer una SPA en React, integrando frontend, backend, BBDD, y una extensión para el navegador.`,
+    titulo: `ANOTÁ! (Another Note Taking App) & Highlighter`,
+    bajada: `Otra aplicación más para tomar notas. Pero con algunas características que ya las quisiera tener en Google Keep. Y lo mejor:  incorpora una extensión para el navegador (highlighter) que permite resaltar y guardar textos y enlaces desde cualquier web.`,
     stack: (
       <ul>
         <li>React / JavaScript</li>
         <li>HTML / CSS</li>
         <li>Node.js / Express</li>
         <li>MySQL</li>
-        <li>Chrome API</li>
+        <li>WebExtension API</li>
+        <li>JSDoc</li>
       </ul>
     ),
     linkCodeText: {
       front: `frontend`,
       back: `backend`,
-      ext: `extensión`,
+      ext: `highlighter`,
     },
     linkCodeHref: {
       front: `https://github.com/fedeholc/anota-front-react`,
       back: `https://github.com/fedeholc/anota-back-express`,
-      ext: `https://github.com/fedeholc/anota-chrome-extension`,
+      ext: `https://github.com/fedeholc/anota-highlighter`,
     },
     linkWebText: ``,
     linkWebHref: ``,
-    subtitulo1: `¿Para qué sirve?`,
-    texto1: `¡Evidentemente que para escribir y organizar notas de texto! =)`,
+    subtitulo1: ``,
+    texto1: ``,
     subtitulo2: `¿Qué tiene de bueno?`,
     texto2: (
       <ul>
@@ -54,15 +55,12 @@ export default function ProyectoANOTA() {
           notas por texto en su título o contenido.
         </li>
         <li>
-          La extensión del navegador permite guardar en una nota el enlace de la
-          página que se está viendo, o guardar todas las páginas abiertas en una
-          sesión.
-        </li>
-        <li>
-          Algunas de las características pensadas para incorporarle son: poder
-          reordenar las notas según distintos criterios; capacidad de darle
-          formato al texto de la nota utilizando Markdown; que la extensión de
-          chrome pueda enviar fragmentos seleccionados de texto.
+          La extensión para el navegador (Highlighter) permite que simplemente
+          al realizar una selección de texto en cualquier web, el texto se
+          resalte y se guarde. Luego todos los fragmentos de texto seleccionados
+          de una misma URL se pueden enviar a ANOTA y/o ser descargados en un
+          archivo de texto. También se pueden generar notas guardando el enlace
+          de la web, o toda la sesion de enlaces abiertos en el navegador.
         </li>
       </ul>
     ),
@@ -89,6 +87,18 @@ export default function ProyectoANOTA() {
           librerías, pero me quisiera poder estudiar en profundidad esos
           problemas para poder implementar mis propias soluciones ya que son
           aspectos importantes del proyecto.
+        </li>
+        <li>
+          En cuanto a la extensión del navegador, no fue nada fácil, dado que
+          recientemente se cambió al manifest V3 (lo cual conlleva numerosas
+          modificaciones en las APIs), y no abundan los materiales y ejemplos de
+          código actualizados. Pero valió la pena el esfuerzo porque trabajar en
+          una aplicación que tiene que manejar directamente el DOM me obligó a
+          reforzar los fundamentos de JavaScript. Incluso me llevo a estudiar
+          sobre estructuras de datos para poder resolver un problema que era
+          recorrer el arbol del DOM desde el nodo de inicio de la selección de
+          texto hasta el nodo final, para poder obtener el texto, resaltarlo,
+          etc.
         </li>
         <li>
           Siendo esta la aplicación más grande que he realizado hasta ahora, me
@@ -119,6 +129,12 @@ export default function ProyectoANOTA() {
           Utilicé CSS modules por componentes, junto con la metodología BEM, con
           la intención de tener un código CSS claro y mantenible, separado de la
           lógica de la aplicación.
+        </li>
+        <li>
+          La extensión está en puro HTML, CSS, y JavaScript. Pero decidí
+          incorporar JSDoc para aprender a utilizarlo, tener un código mejor
+          documentado, y también poder tener type check a través del language
+          server de typescript.
         </li>
       </ul>
     ),
@@ -180,9 +196,9 @@ export default function ProyectoANOTA() {
         </div>
         <br />
         {/* ***************************************** */}
-        <h3>{TEXTO.subtitulo1}</h3>
+        {/*   <h3>{TEXTO.subtitulo1}</h3> */}
         <div className={proyectos.columns1}>
-          <p>{TEXTO.texto1}</p>
+          {/* <p>{TEXTO.texto1}</p> */}
           <div className={proyectos.screenshot__container}>
             <figure className={`${proyectos.screenshot__figure}  `}>
               <Image
@@ -190,7 +206,7 @@ export default function ProyectoANOTA() {
                 src={screenshot1}
                 alt="screenshot"
                 quality={100}
-                width={700}
+                width={1024}
               />
               <figcaption className={proyectos.screenshot__caption}>
                 Muestra de las distintas posibilidades de visualización de las
@@ -211,10 +227,10 @@ export default function ProyectoANOTA() {
                 src={screenshot2}
                 alt="screenshot"
                 quality={100}
-                width={700}
+                width={1024}
               />
               <figcaption className={proyectos.screenshot__caption}>
-                Funcionamiento de la extensión para ell navegador.
+                Funcionamiento de la extensión para el navegador.
               </figcaption>
             </figure>
           </div>
@@ -231,7 +247,7 @@ export default function ProyectoANOTA() {
                 src={screenshot3}
                 alt="screenshot"
                 quality={100}
-                width={700}
+                width={1024}
               />
               <figcaption className={proyectos.screenshot__caption}>
                 Muestra de la creación de notas y filtrado por etiquetas.
