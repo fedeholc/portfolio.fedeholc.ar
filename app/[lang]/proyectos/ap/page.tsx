89642ea5b5b53af11cb2137b5c0ccb551c5b668c
fedeholc <federicoholc@gmail.com>
2024-02-17 19:59:21 -0300

    Add Spanish translations for Argentina Programa v3.0 - Resume

--- app/[lang]/proyectos/ap/page.tsx
+++ app/[lang]/proyectos/ap/page.tsx
@@ -15,11 +15,15 @@ import { Locale } from "@app/i18n-config";
 
 // La elección de las tecnologías no fue propia sino que fueron las que aprendimos en el curso.
 
-export default async function ProyectoAP({ params: { lang } }: { params: { lang: Locale } }) {
+export default async function ProyectoAP({
+  params: { lang },
+}: {
+  params: { lang: Locale };
+}) {
   const dictionary = await getDictionary();
   const TEXTO = {
-    titulo: `Argentina Programa v3.0 - Curriculum Vitae`,
-    bajada: `Es un sitio web a modo de curriculum vitae online que integra distintas tecnologías y funcionalidades. Fue solicitado como trabajo final del curso de desarrollo web full stack de Argentina Programa. Aprendí mucho haciéndolo, pero más importante aún, me permitió reencontrarme felizmente con la programación. =)`,
+    titulo: dictionary.proyectos.ap01[lang],
+    bajada: dictionary.proyectos.ap02[lang],
     stack: (
       <ul>
         <li>Angular / TypeScript / JavaScript</li>
@@ -39,50 +43,21 @@ export default async function ProyectoAP({ params: { lang } }: { params: { lang:
     },
     linkWebText: `https://portfolioap-cccf4.web.app/`,
     linkWebHref: `https://portfolioap-cccf4.web.app/`,
-    subtitulo1: `Objetivo`,
-    texto1: `-`,
-    subtitulo2: `¿Qué tiene de bueno?`,
+    subtitulo2: dictionary.proyectos.ap03[lang],
     texto2: (
       <ul>
-        <li>Diseño responsive, adaptable a distintos tamaños de pantalla.</li>
-        <li>Autorización de usuario para acceder a la modalidad de edición.</li>
-        <li>
-          Posibilidad de editar los elementos a mostrar en cada una de las
-          secciones, tanto en lo que hace al texto como a las imágenes.
-        </li>
+        <li>{dictionary.proyectos.ap04[lang]}</li>
+        <li>{dictionary.proyectos.ap05[lang]}</li>
+        <li>{dictionary.proyectos.ap06[lang]}</li>
       </ul>
     ),
-    subtitulo3: "Problemas en el camino y aprendizajes",
+    subtitulo3: dictionary.proyectos.ap07[lang],
     texto3: (
       <ul>
-        <li>
-          No fue sencillo armar el rompecabezas de tecnologías necesarias para
-          que todo funcione, y en algunos aspectos las partes quedaron
-          &quot;atadas con alambre&quot;, ya que no tenía el tiempo necesario
-          para profundizar en las bases de cada herramienta y encontrar la mejor
-          solución posible. ¡Pero funcionó!
-        </li>
-        <li>
-          El trabajo me permitió tener una visión general de cómo se integra un
-          desarrollo que incluye un front end y un back end con API y base de
-          datos.
-        </li>
-        <li>
-          Con Angular comprendí cómo era trabajar con un framework para crear
-          una SPA, y en particular cómo se estructura el código utilizando
-          componentes y cómo se implementa un patrón de diseño MVC. El haber
-          aprendido directamente Angular con TypeScript me obligo a tener que
-          estudiar JavaScrip a posteriori para poder tener un conocimiento de
-          los fundamentos y poder diferenciar que características eran propias
-          de cada tecnología.
-        </li>
-        <li>
-          La exigencia de hacer el deploy en un sitio en particular, y que este
-          fuera cambiando varias veces (Heroku, Fly.io, Koyeb) cuando faltaba
-          poco tiempo para la entrega fue un dolor de cabeza. Pero rescato que
-          me permitió conocer distintas opciones y formas de hacer deploys de
-          las aplicaciones.
-        </li>
+        <li>{dictionary.proyectos.ap08[lang]}</li>
+        <li>{dictionary.proyectos.ap09[lang]}</li>
+        <li>{dictionary.proyectos.ap10[lang]}</li>
+        <li>{dictionary.proyectos.ap11[lang]}</li>
       </ul>
     ),
   };
@@ -129,10 +104,7 @@ export default async function ProyectoAP({ params: { lang } }: { params: { lang:
         </div>
         <br />
         {/* ***************************************** */}
-        {/*  <h3>{TEXTO.subtitulo1}</h3> */}
         <div className={proyectos.columns1}>
-          {/*           <p>{TEXTO.texto1}</p>
-           */}{" "}
           <div className={proyectos.screenshot__container}>
             <figure className={`${proyectos.screenshot__figure} `}>
               <Image
@@ -160,8 +132,7 @@ export default async function ProyectoAP({ params: { lang } }: { params: { lang:
                 quality={100}
               />
               <figcaption className={proyectos.screenshot__caption}>
-                Vista de una de las secciones con los botones de edición
-                activados.
+                {dictionary.proyectos.ap12[lang]}
               </figcaption>
             </figure>
           </div>
@@ -175,7 +146,7 @@ export default async function ProyectoAP({ params: { lang } }: { params: { lang:
                 quality={100}
               />
               <figcaption className={proyectos.screenshot__caption}>
-                Vista de la ventana de edición de la portada.
+                {dictionary.proyectos.ap13[lang]}
               </figcaption>
             </figure>
           </div>
@@ -195,7 +166,7 @@ export default async function ProyectoAP({ params: { lang } }: { params: { lang:
                 quality={100}
               />
               <figcaption className={proyectos.screenshot__caption}>
-                Portada del sitio web visto en un teléfono.
+                {dictionary.proyectos.ap14[lang]}
               </figcaption>
             </figure>
           </div>
