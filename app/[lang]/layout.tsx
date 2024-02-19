9d632348521ac72c53825852f4337059a54688b3
fedeholc <federicoholc@gmail.com>
2024-02-19 12:35:55 -0300

    Add open graph metadata to layout.tsx

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -37,6 +37,27 @@ export const metadata = {
   icons: {
     icon: "/favicon.ico",
   },
+  openGraph: {
+    title: "Next.js",
+    description: "The React Framework for the Web",
+    url: "https://nextjs.org",
+    siteName: "Next.js",
+    images: [
+      {
+        url: "/public/opengraph-image.jpeg",
+        width: 800,
+        height: 600,
+      },
+      {
+        url: "https://nextjs.org/og-alt.png",
+        width: 1800,
+        height: 1600,
+        alt: "My custom alt",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 };
 
 export async function generateStaticParams() {
