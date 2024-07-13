727866c7b71cf9b966d3191ad75a4889a62d4433
Federico Holc <federicoholc@gmail.com>
2024-07-13 13:27:42 -0300

    add other projects

--- app/[lang]/proyectos/page.tsx
+++ app/[lang]/proyectos/page.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 
 import { CardsProyectos } from "@app/[lang]/components/cardProyecto";
 import herb from "@public/herb.png";
+import externalLinkIcon from "@public/external-link.png";
 
 import ProyectoANOTA from "./anota/page";
 import ProyectoAP from "./ap/page";
@@ -16,6 +17,7 @@ import GoToTop from "@app/[lang]/components/goToTop";
 import { type Locale } from "@app/i18n-config";
 import { getDictionary } from "@app/get-dictionary";
 import { Metadata } from "next";
+import Link from "next/link";
 
 /*         👾🌿🍁🪐🌟🛠️💘🕸️🚲️🎼⭐
  */
@@ -62,15 +64,72 @@ async function ProyectosTodos({ lang }: { lang: Locale }) {
         <div className={proyectos.header}>
           {dictionary.proyectos.proyectos[lang]}
         </div>
-
         <div className={proyectos.divider}>
           <Image src={herb} alt="herb emoji" width={26} quality={100} />
         </div>
-
         <div className={proyectos.cards__container}>
           <CardsProyectos lang={lang} />
+        </div>{" "}
+        <div className={proyectos.divider}>
+          <Image src={herb} alt="herb emoji" width={26} quality={100} />
         </div>
-
+        <div className={proyectos.proyecto__container}>
+          <h3>{dictionary.proyectos.otros[lang]}</h3>
+          <div className={proyectos.columns1}>
+            <div>
+              <ul>
+                <li className={proyectos.smaller_font}>
+                  <div className={proyectos.link_icon}>
+                    <span>
+                      {dictionary.proyectos.otros1[lang]}&nbsp;
+                      <Link
+                        className={proyectos.underline}
+                        target="_blank"
+                        href="https://github.com/fedeholc/tecnicatura-tp-clinica"
+                      >
+                        {dictionary.proyectos.otrosVerCodigo[lang]}
+                      </Link>
+                      &nbsp;
+                      <Image src={externalLinkIcon} alt="external link icon" />
+                    </span>
+                  </div>
+                </li>
+                <li className={proyectos.smaller_font}>
+                  <div className={proyectos.link_icon}>
+                    <span>
+                      {dictionary.proyectos.otros2[lang]}&nbsp;
+                      <Link
+                        className={proyectos.underline}
+                        target="_blank"
+                        href="https://github.com/fedeholc/tecnicatura-tp-club-deportivo-mobile"
+                      >
+                        {dictionary.proyectos.otrosVerCodigo[lang]}
+                      </Link>
+                      &nbsp;
+                      <Image src={externalLinkIcon} alt="external link icon" />
+                    </span>
+                  </div>
+                </li>
+                <li className={proyectos.smaller_font}>
+                  <div className={proyectos.link_icon}>
+                    <span>
+                      {dictionary.proyectos.otros3[lang]}&nbsp;
+                      <Link
+                        className={proyectos.underline}
+                        target="_blank"
+                        href="https://github.com/fedeholc/argentinaprograma-Portfolio-Backend"
+                      >
+                        {dictionary.proyectos.otrosVerCodigo[lang]}
+                      </Link>
+                      &nbsp;
+                      <Image src={externalLinkIcon} alt="external link icon" />
+                    </span>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
         <div className={proyectos.divider}>
           <Image src={herb} alt="herb emoji" width={26} quality={100} />
         </div>
