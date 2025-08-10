d423fd2d880127f273f6e4511f53eb52bec8d42f
Federico Holc <federicoholc@gmail.com>
2025-08-10 12:57:50 -0300

    Refactor GoToTop component and update hover styles

--- app/[lang]/components/goToTop.tsx
+++ app/[lang]/components/goToTop.tsx
@@ -5,7 +5,9 @@ import goToTop from "./goToTop.module.css";
 export default function GoToTop() {
   return (
     <button className={goToTop.goTop}>
-      <Link href="/proyectos/#inicio">&#x279C;</Link>
+      <Link href="/proyectos/#inicio">
+        <div style={{ marginTop: "-3px" }}>&#x279C;</div>
+      </Link>
     </button>
   );
 }
